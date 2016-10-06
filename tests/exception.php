1ce8e9e290643ca86a7672434b2a47574f1d7643
John Cupitt <jcupitt@gmail.com>
2016-10-06 14:53:35 +0100

    add some tests for exceptions

---
+++ tests/exception.php
@@ -0,0 +1,67 @@
+<?php
+
+use Jcupitt\Vips;
+
+class VipsExceptionTest extends PHPUnit_Framework_TestCase 
+{
+    protected function setUp()
+    {
+        $filename = dirname(__FILE__) . "/images/img_0076.jpg";
+        $this->image = Vips\Image::newFromFile($filename);
+
+        /* The original value of pixel (0, 0).
+         */
+        $this->pixel = $this->image->getpoint(0, 0);
+    }
+
+    public function testVipsNewFromFileException()
+    {
+        $this->expectException(Exception::class);
+
+        $image = Vips\Image::newFromFile("I don't exist.jpg");
+    }
+
+    public function testVipsWriteToFileException()
+    {
+        $this->expectException(Exception::class);
+
+        $this->image->writeToFile("/permissions error.jpg");
+    }
+
+    public function testVipsWriteToBufferException()
+    {
+        $this->expectException(Exception::class);
+
+        $string = $this->image->writeToBuffer(".jpg", ["crazy option" => 42]);
+    }
+
+    public function testVipsGetException()
+    {
+        $this->expectException(Exception::class);
+
+        $x = $this->image->get("I don't exist");
+    }
+
+    /* 
+     *
+     * oop this causes a segv
+     *
+    public function testVipsOperationException()
+    {
+        $this->expectException(Exception::class);
+
+        $x = $this->image->add("nonsense");
+    }
+     *
+     */
+
+}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: expandtab sw=4 ts=4 fdm=marker
+ * vim<600: expandtab sw=4 ts=4
+ */
