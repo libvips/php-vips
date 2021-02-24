96dde84bc6e9ebad0f8bebe1a742cca4c5e123ee
John Cupitt <jcupitt@gmail.com>
2021-02-24 09:36:13 +0000

    watermark formatting

--- examples/watermark-text.php
+++ examples/watermark-text.php
@@ -4,14 +4,14 @@
 require __DIR__ . '/vendor/autoload.php';
 use Jcupitt\Vips;
 
-if(count($argv) != 4) {
-    echo("usage: ./watermark-text.php input-image output-image \"some text\"\n");
+if (count($argv) != 4) {
+    echo("usage: ./watermark-text.php input output \"some text\"\n");
     exit(1);
 }
 
 // we can stream the main image, and we want all frames
 $image = Vips\Image::newFromFile($argv[1], [
-  'access' => 'sequential', 
+  'access' => 'sequential',
   'n' => -1
 ]);
 
@@ -21,7 +21,10 @@ $text = $argv[3];
 // the size of each frame
 $page_height = $image->get('page-height');
 
-$text_mask = Vips\Image::text($text, ['width' => $image->width, 'dpi' => 150]);
+$text_mask = Vips\Image::text($text, [
+  'width' => $image->width,
+  'dpi' => 150
+]);
 
 // semi-transparent white text on a blue background
 $foreground = [255, 255, 255, 50];
@@ -30,20 +33,33 @@ $background = [0, 0, 255, 50];
 // and a 10-pixel marghin
 $margin = 10;
 
-$overlay = $text_mask->ifthenelse($foreground, $background, ['blend' => TRUE ]);
+$overlay = $text_mask->ifthenelse($foreground, $background, [
+  'blend' => true
+]);
 
 // add a margin, with the same background
-$overlay = $overlay->embed($margin, $margin, 
-  $overlay->width + 2 * $margin, $overlay->height + 2 * $margin, 
-  ['extend' => 'background', 'background' => $background]);
+$overlay = $overlay->embed(
+    $margin,
+    $margin,
+    $overlay->width + 2 * $margin,
+    $overlay->height + 2 * $margin,
+    [
+        'extend' => 'background',
+        'background' => $background
+    ]
+);
 
 // tag as srgb
 $overlay = $overlay->copy(['interpretation' => 'srgb']);
 
 // expand to the size of a frame, transparent background, place at the bottom
 // left
-$overlay = $overlay->embed($margin, $page_height - $overlay->height - $margin, 
-  $image->width, $page_height);
+$overlay = $overlay->embed(
+    $margin,
+    $page_height - $overlay->height - $margin,
+    $image->width,
+    $page_height
+);
 
 // expand to the full size of the gif roll
 $overlay = $overlay->replicate(1, $image->height / $page_height);
