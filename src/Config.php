ff75115c2087b33e7485725164c4063ba1b285c9
John Cupitt <jcupitt@gmail.com>
2022-03-07 15:21:26 +0000

    Update src/Config.php
    Co-authored-by: Kleis Auke Wolthuizen <github@kleisauke.nl>

--- src/Config.php
+++ src/Config.php
@@ -382,9 +382,6 @@ void g_free (void* data);
 
 void vips_leak_set (int leak);
 
-char* vips_path_filename7 (const char* path);
-char* vips_path_mode7 (const char* path);
-
 GType vips_type_find (const char* basename, const char* nickname);
 const char* vips_nickname_find (GType type);
 
