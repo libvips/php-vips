e76405628f3075459c7cbbe287e6a085ce88fe08
John Cupitt <jcupitt@gmail.com>
2023-05-03 08:51:46 +0100

    more examples

---
+++ examples/progress.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+use Jcupitt\Vips;
+
+#Vips\Config::setLogger(new Vips\DebugLogger());
+
+$image = Vips\Image::black(1, 1000000);
+$image->setProgress(true);
+
+$image->signalConnect("preeval", function($image, $progress) {
+    echo "preeval:\n";
+});
+$image->signalConnect("eval", function($image, $progress) {
+    echo "eval: $progress->percent % complete\r";
+});
+
+$image->signalConnect("posteval", function($image, $progress) {
+    echo "\nposteval:\n";
+});
+
+// trigger evaluation
+$image->avg();
+
+$image = null;
+
+Vips\FFI::shutDown();
