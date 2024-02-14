439b280d22213a2b5c7c76251d7af57734d0c3dc
John Cupitt <jcupitt@gmail.com>
2024-02-14 16:53:14 +0000

    oop, for the condition

--- src/FFI.php
+++ src/FFI.php
@@ -243,7 +243,7 @@ class FFI
         if (!ini_get('ffi.enable')) {
             throw new Exception("ffi.enable not set to 'true'");
         }
-        if (version_compare(PHP_VERSION, '8.3') &&
+        if (version_compare(PHP_VERSION, '8.3', '>=') &&
             ini_get('zend.max_allowed_stack_size') != '-1') {
             throw new Exception("zend.max_allowed_stack_size not set to '-1'");
         }
